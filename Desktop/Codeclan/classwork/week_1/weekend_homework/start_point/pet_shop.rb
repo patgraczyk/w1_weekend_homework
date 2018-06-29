25632bcf671d6cb2f7491357732b7fad9a550b0a
Pat <graczyk.pat@gmail.com>
2018-06-29 15:32:16 +0100

    file set up and first 3 activities

---
+++ Desktop/Codeclan/classwork/week_1/weekend_homework/start_point/pet_shop.rb
@@ -0,0 +1,19 @@
+#1 test_pet_shop_name
+
+def pet_shop_name (name)
+  return name[:name]
+end
+
+#2 test_total_cash
+
+def total_cash(place)
+  return place[:admin][:total_cash]
+end
+
+#3test_add_or_remove_cash__add
+
+
+#7 test_stock_count
+def stock_count(place)
+  return @pet_shop[:pets].length
+end
