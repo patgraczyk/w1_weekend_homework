467320f78b45ad942c1b2c3b6a6916f92d5fe7f9
Pat <graczyk.pat@gmail.com>
2018-06-30 13:57:18 +0100

    12 activities completed

--- Desktop/Codeclan/classwork/week_1/weekend_homework/start_point/pet_shop.rb
+++ Desktop/Codeclan/classwork/week_1/weekend_homework/start_point/pet_shop.rb
@@ -66,6 +66,16 @@ def pets_by_breed(pet_shop, breed)
   return breed_type
 end
 
+#10 find pet by name returns nill
+
+# def find_pet_by_name(pet_shop, name)
+#   for pet in pet_shop[:pets][:name]
+#     if pet[:name] == pet
+#       return
+#     end
+#   end
+# end
+
 #11 find pet by name returns nill
 
 def find_pet_by_name(pet_shop, name)
@@ -76,7 +86,32 @@ def find_pet_by_name(pet_shop, name)
 end
 end
 
-# #17 add pet to the customer
+# 12 def test_remove_pet_by_name
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet
+      delete(name)
+  end
+end
+end
+
+#13 add pet to stock
+
+def add_pet_to_stock(pet_shop, new_pet)
+   pet_shop[:pets].push(new_pet)
+end
+
+#14 customer cash
+# def customer_cash(customers,[])
+#     customers[0][:cash]
+# end
+# #15 customer pet count
+# def customer_pet_count (customers [value])
+
+
+
+# #16 add pet to the customer
 # # def add_pet_to_customer (customer, new_pet)
 # #   customers[0] << [@new_pet]
 # #   return @customers[0][:pets].length
